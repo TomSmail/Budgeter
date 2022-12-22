7571d084f6f4193dfa8a79df063487f4d2a58256
TomSmail <thomas.c.smail@gmail.com>
2022-12-22 19:39:19 +0000

    Initial Commit

---
+++ Budgeter/BudgeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgeterApp.swift
+//  Budgeter
+//
+//  Created by Tom Smail on 22/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BudgeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
